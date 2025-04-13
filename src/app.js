a6ba55db411e4bc89b4453d8679636aea7d47051
Maneesh <mannycollege2022@gmailcom>
2025-04-13 14:50:37 +0530

    "whole setup done "

--- src/app.js
+++ src/app.js
@@ -15,4 +15,12 @@ app.use(express.static("public"))
 
 app.use(cookieParser())
 
+
+//routes import 
+import userRouter from './routes/user.routes.js'
+
+//routes declaration
+// direct app.get nhi likh rhe, we need to use middleware yahi syntax hai
+app.use("/api/v1/users", userRouter)
+//http://localhost:8000/api/v1/users/register
 export { app }
